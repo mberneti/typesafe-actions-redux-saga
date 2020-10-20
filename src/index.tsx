0960a57ea6af56e3de1ad408c6ca300b4c8714e5
Mohammadreza Berneti <mberneti@live.com>
2020-10-20 19:16:53 +0330

    fix: removed useless react-scripts-ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,29 @@
 // tslint:disable no-import-side-effect
 // tslint:disable no-submodule-imports
 // This must be the first line in src/index.js
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
-import 'tslib';
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
+import "tslib";
 
-import * as React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
+import * as React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
 
-import Home from './routes/home';
-import store from './store';
+import Home from "./routes/home";
+import store from "./store";
 
 const styles: React.CSSProperties = {
-  fontFamily: 'sans-serif',
-  textAlign: 'center',
+  fontFamily: "sans-serif",
+  textAlign: "center",
 };
 
 const Root = () => (
   <div style={styles}>
-    <h1>asd assa</h1>
+    <h1>Hubber</h1>
     <Provider store={store}>
       <Home />
     </Provider>
   </div>
 );
 
-render(<Root />, document.getElementById('root'));
+render(<Root />, document.getElementById("root"));
